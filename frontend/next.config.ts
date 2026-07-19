2ea6e0055d9072de7e1055d3e5b64ec4601ad826
Pratyush Mohanty <pratyushmohanty120@gmail.com>
2026-07-19 12:09:44 +0530

    fix(frontend): update api client to use next.js rewrites to fix cors and vercel deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   output: "standalone",
   compiler: {
     removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error", "warn"] } : false,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/v1/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/api/v1/:path*` 
+          : "http://127.0.0.1:8000/api/v1/:path*",
+      },
+    ];
   }
 };
 
